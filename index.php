758988adffc197d0d9cc3972d10901b49035deb9
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-10 17:46:23 -0300

    :wrench: chore:  Configuração Head

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <script src="./bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/d6ea71334b.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
